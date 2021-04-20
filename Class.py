06f8bd5de9c1785786f5652782b4850b2d025d00
makskostyshen <maxxxymko2003@gmail.com>
2021-04-20 19:22:47 +0300

    make examples

---
+++ Class.py
@@ -0,0 +1,1 @@
+"""created by Kostyshen Maksym"""
---
+++ main.py
@@ -0,0 +1,22 @@
+"""created by Kostyshen Maksym"""
+
+def pr_exec() -> None:
+    """
+    Output information about executor
+    """
+    print("Kostyshen Maksym, K12")
+    print("80 variant")
+
+def pr_cond():
+    """
+    Output information about conditions of a task
+    """
+    print("Use the data from the semester register and find students, who skipped more than 15 lectures.")
+    print("Write required information about them in definite file.")
+separ = "*****"
+
+
+pr_exec()
+pr_cond()
+
+def main(): pass
